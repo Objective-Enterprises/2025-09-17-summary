769b3b5d8c2a1f49aab966f11034125ecdf0f6c0
David Y. Stephenson <david@davidystephenson.com>
2025-08-17 12:02:50 -0400

    Map over people

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import Hello from "./Hello"
 
 export default function App () {
+  const people = [
+    { person: 'Zelda', greeting: 'Hello' },
+    { person: 'Dorothy', greeting: 'Hi', },
+    { person: 'Tallulah', greeting: 'Wassup' },
+    { person: 'Dorothy', greeting: 'Hello', },
+  ]
+  const greetings = people.map((element, index) => {
+    return (
+      <Hello key={index} person={element.person} greeting={element.greeting} />
+    )
+  })
   return (
     <>
       <h1 id='heading'>App</h1>
       <p>Welcome to the app</p>
-      <Hello person='Zelda' greeting='Hello' />
-      <Hello person='Dorothy' greeting='Hi' />
-      <Hello person='Tallulah' greeting='Wassup' />
+      {greetings}
     </>
   )
 }
